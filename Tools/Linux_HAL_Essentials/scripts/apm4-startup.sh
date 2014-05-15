367b704ff27b0a86b28184098036a1cefa421ee5
root <root@beaglebone.(none)>
2014-05-15 03:06:54 +0000

    Update startup script

--- Tools/Linux_HAL_Essentials/scripts/apm4-startup.sh
+++ Tools/Linux_HAL_Essentials/scripts/apm4-startup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# Script location
+#This script is launched automatically in the BeagleBone Black
+#on every boot and loads everything that the board needs to
+#act as an autopilot. The COPY_CAPES environment variable copies
+#the capies on every boot if set to 1.
+#
+# Victor Mayoral Vilches - Erle Robotics [victor@erlerobot.com]
 
+# Cape source location
 LOCATION="/root/ardupilot/Tools/Linux_HAL_Essentials"
 
-echo "Loading Test_Capes..."
-cp $LOCATION/BB-SPI0-PXF-01-00A0.dtbo /lib/firmware/
-cp $LOCATION/BB-SPI1-PXF-01-00A0.dtbo /lib/firmware/
-cp $LOCATION/BB-BONE-PRU-05-00A0.dtbo /lib/firmware/
-cp $LOCATION/rcinpru0 /lib/firmware
-cp $LOCATION/pwmpru1 /lib/firmware
+# Copy capes
+COPY_CAPES=0
+
+if (($COPY_CAPES == 1)); then 
+	cp $LOCATION/BB-SPI0-PXF-01-00A0.dtbo /lib/firmware/
+	cp $LOCATION/BB-SPI1-PXF-01-00A0.dtbo /lib/firmware/
+	cp $LOCATION/BB-BONE-PRU-05-00A0.dtbo /lib/firmware/
+	cp $LOCATION/rcinpru0 /lib/firmware
+	cp $LOCATION/pwmpru1 /lib/firmware
+fi 
 
 # Loading the capes
 echo BB-BONE-PRU-05 > /sys/devices/bone_capemgr.*/slots
@@ -20,12 +30,15 @@ echo BB-UART4 > /sys/devices/bone_capemgr.*/slots
 echo BB-UART2 > /sys/devices/bone_capemgr.*/slots
 
 # Line for making PREEMPT_RT work
-echo 0:rcinpru0 > /sys/devices/ocp.3/4a300000.prurproc/load
+#echo 0:rcinpru0 > /sys/devices/ocp.3/4a300000.prurproc/load
 
 # Logging
 dmesg | grep "SPI"
 dmesg | grep "PRU"
 cat /sys/devices/bone_capemgr.*/slots
+
+# Force the ip address of the hotspot
+ifconfig wlan2 11.0.0.1
 
 cd /root
 (
@@ -33,12 +46,11 @@ cd /root
     init 3
     #killall -q udhcpd
     while :; do
-        # Set CPU at max speed
-        cpufreq-set -f 1000MHz
-
-    	# Start copter, modify if other vehicle is needed
+	# Set CPU at max speed
+	cpufreq-set -f 1000MHz
+	# Start copter, modify if other vehicle is needed
         #./ArduCopter.elf -A /dev/ttyO0 -B /dev/ttyO5
-	    ./ArduCopter.elf -A tcp:*:6000:wait -B /dev/ttyO5
+	./ArduCopter.elf -A tcp:*:6000:wait -B /dev/ttyO5
     done
 ) >> ~/copter.log 2>&1
 
