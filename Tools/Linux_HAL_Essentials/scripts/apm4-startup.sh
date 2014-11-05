44f342db1bab7a351b0f7ba857b54c304ed622d4
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2014-11-04 22:47:33 -0800

    HAL_Linux: Update startup scripts

--- Tools/Linux_HAL_Essentials/scripts/apm4-startup.sh
+++ Tools/Linux_HAL_Essentials/scripts/apm4-startup.sh
@@ -14,11 +14,11 @@ LOCATION="/root/ardupilot/Tools/Linux_HAL_Essentials"
 COPY_CAPES=0
 
 if (($COPY_CAPES == 1)); then 
-	cp $LOCATION/BB-SPI0-PXF-01-00A0.dtbo /lib/firmware/
-	cp $LOCATION/BB-SPI1-PXF-01-00A0.dtbo /lib/firmware/
-	cp $LOCATION/BB-BONE-PRU-05-00A0.dtbo /lib/firmware/
-	cp $LOCATION/rcinpru0 /lib/firmware
-	cp $LOCATION/pwmpru1 /lib/firmware
+    cp $LOCATION/BB-SPI0-PXF-01-00A0.dtbo /lib/firmware/
+    cp $LOCATION/BB-SPI1-PXF-01-00A0.dtbo /lib/firmware/
+    cp $LOCATION/BB-BONE-PRU-05-00A0.dtbo /lib/firmware/
+    cp $LOCATION/rcinpru0 /lib/firmware
+    cp $LOCATION/pwmpru1 /lib/firmware
 fi 
 
 # Loading the capes
@@ -28,6 +28,8 @@ echo BB-SPI1-PXF-01 > /sys/devices/bone_capemgr.*/slots
 echo BB-UART5 > /sys/devices/bone_capemgr.*/slots
 echo BB-UART4 > /sys/devices/bone_capemgr.*/slots
 echo BB-UART2 > /sys/devices/bone_capemgr.*/slots
+echo am33xx_pwm > /sys/devices/bone_capemgr.*/slots
+echo bone_pwm_P8_36 > /sys/devices/bone_capemgr.*/slots
 
 # Line for making PREEMPT_RT work
 #echo 0:rcinpru0 > /sys/devices/ocp.3/4a300000.prurproc/load
@@ -40,17 +42,22 @@ cat /sys/devices/bone_capemgr.*/slots
 # Force the ip address of the hotspot
 ifconfig wlan2 11.0.0.1
 
+# Give the system time to load all the capes
+#   experienced has proved that generally the buzzer needs some time
+sleep 1
+
 cd /root
 (
     date
     init 3
     #killall -q udhcpd
     while :; do
-	# Set CPU at max speed
-	cpufreq-set -f 1000MHz
-	# Start copter, modify if other vehicle is needed
+    # Set CPU at max speed
+    cpufreq-set -f 1000MHz
+    # Start copter, modify if other vehicle is needed
+    ./ArduCopter.elf -A udp:192.168.7.1:6000 -B /dev/ttyO5
+    #./ArduCopter.elf -A udp:11.0.0.2:6000 -B /dev/ttyO5
         #./ArduCopter.elf -A /dev/ttyO0 -B /dev/ttyO5
-	./ArduCopter.elf -A tcp:*:6000:wait -B /dev/ttyO5
+    #./ArduCopter.elf -A tcp:*:6000:wait -B /dev/ttyO5
     done
 ) >> ~/copter.log 2>&1
-
